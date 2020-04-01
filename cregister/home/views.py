0aef1587b654454af6a149d31c385d13efed25f9
adilu khalid <adiluabdul@gmail.com>
2020-04-01 23:56:30 +0300

    Detlt with home page view

--- cregister/home/views.py
+++ cregister/home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .models import ClientIssues, CaseType
 def index(request):
-	return render(request, 'home/home.html')
+	caseTypes = CaseType.objects.all()
+	issues = ClientIssues.objects.all()
+	context = {
+		'caseTypes': caseTypes,
+		'issues': issues,
+	}
+	return render(request, 'home/home.html', context)
