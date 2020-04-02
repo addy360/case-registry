def973ea2968255882dcccdd74933fe748c13bb1
adilu khalid <adiluabdul@gmail.com>
2020-04-02 14:48:14 +0300

    Added a message alert functionality and submission of casetype and issue

--- cregister/home/urls.py
+++ cregister/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, issue, caseType
 
 urlpatterns = [
     path('', index, name='home' ),
+    path('issue', issue, name='issue' ),
+    path('caseType', caseType, name='caseType' ),
 ]
--- cregister/home/views.py
+++ cregister/home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import ClientIssues, CaseType
 def index(request):
@@ -9,3 +10,25 @@ def index(request):
 		'issues': issues,
 	}
 	return render(request, 'home/home.html', context)
+
+def issue(request):
+	if request.method == 'POST':
+		iss = request.POST['issueType']
+		if iss == '':
+			messages.add_message(request, messages.ERROR, 'Please enter something!')
+		else:		
+			ClientIssues.objects.create(name=iss)
+			messages.add_message(request, messages.SUCCESS, f'You have added "{iss}" successfully!')
+	return redirect('/')
+
+def caseType(request):
+	if request.method == 'POST':
+		iss = request.POST['caseType']
+		if iss == '':
+			messages.add_message(request, messages.ERROR, 'Please enter something!')
+		else:		
+			CaseType.objects.create(name=iss)
+			messages.add_message(request, messages.SUCCESS, f'You have added "{iss}" successfully!')
+	return redirect('/')
+
+
